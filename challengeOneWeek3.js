daf8f141501cb9dc7c58a96e8bb5d2e5e53824f5
reenesa <susannins@gmail.com>
2019-09-09 16:08:21 +0300

    add variables

---
+++ challengeOneWeek3.js
@@ -0,0 +1,15 @@
+
+    var i;
+    var str='1ab2c3defUIoAu1806';
+    var even =[];
+    var odd =[];
+    var numbers = str.match(/\d+/g)
+    const chars = str.toLowerCase().match(/[A-z]/g).sort()
+    var sortedStr=[];
+    for (i=0; i<str.length;i++){
+
+    }
+    // return numbers;
+
+
+   
