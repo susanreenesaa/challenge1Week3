efbeb926a1258219e2048c71a291abff699d0c72
reenesa <susannins@gmail.com>
2019-09-12 22:11:03 +0300

    correct sort array and global variables

--- challengeOneWeek3.js
+++ challengeOneWeek3.js
@@ -1,32 +1,29 @@
 
-    var i;
-    var str='1ab2c3defUIoAu1806';
-    var even =[];
-    var odd =[];
+var str='1ab2c3defUIoAu18x0p6o9i20k100';
+// not yet sure if these should be global
     var numbers = str.match(/\d+/g)
     const chars = str.toLowerCase().match(/[A-z]/g).sort()
-    var sortedStr=[];
-    for (i=0; i<str.length;i++){
-
-    }
-    // return numbers;
-
 
     function evenAndOddNumbers(num){
+        var i;
+        var even =[];
+        var odd =[];
+        var isGreaterThan = function(a, b){return a-b}
         for(i=0; i<num.length; i++){
             if(num[i]%2===0){
                 even.push(num[i])
             }
-            else if(num[i]%2 !=0){
+            else if(num[i]%2 ===1){
                 odd.push(num[i]);
            
         }
         }
         
     return object={
-        even:even.sort(),
-        odd:odd.sort(),
+        even:even.sort(isGreaterThan),
+        odd:odd.sort(isGreaterThan),
         chars:chars
     }
     }
+
 console.log(evenAndOddNumbers(numbers));
